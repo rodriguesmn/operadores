5be789921e1bcd1d1e5df03c4cec2ba898c73e73
rodrigues <manuellar2007@gmail.com>
2024-09-30 21:30:17 -0300

    realização do exercicio 2

---
+++ 1exercicio.js
@@ -0,0 +1,4 @@
+console.log (3+4)
+console.log ( (3*5)/2)
+console.log ((5-4)*-1)
+console.log(234%5)
---
+++ 2exercicio.js
@@ -0,0 +1,7 @@
+let a = 1
+let b = 2
+console.log ("Q1", a===b)
+console.log ("Q2", a!==b)
+console.log ("Q3", a>b)
+console.log ("Q4", a<b)
+
